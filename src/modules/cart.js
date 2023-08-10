eaa11e4680d4b600f1fee8f8eb73e1d380d0f68b
Gyuzal <aidarkhanova.g.03@mail.ru>
2023-08-10 17:14:18 +0200

    first day

---
+++ src/modules/cart.js
@@ -0,0 +1,18 @@
+const cart = () => {
+    const cartBtn = document.getElementById('cart')
+    const cartModal = document.querySelector('.cart')
+    const CartCloseModal = document.querySelector('.cart-close')
+
+    const openCart = () => {
+        cartModal.style.display = 'flex'
+    }
+
+    const closeCart = () => {
+        cartModal.style.display = ''
+    }
+
+    cartBtn.addEventListener('click', openCart)
+    CartCloseModal.addEventListener('click', closeCart)
+}
+
+export default cart
